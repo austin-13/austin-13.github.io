9594e0cddb7e670cd985a4469c321e53df057ebe
Austin Martin <austinm@nmsu.edu>
2025-03-25 14:50:47 -0600

    revert

--- game/connect4.js
+++ game/connect4.js
@@ -1,72 +1,16 @@
-var socket = io(); //connects to the node.js server
-
-socket.emit("joinGame");    //when connected requests to join the game
-
-var playerColor = null; //players color assigned by server
-
 // player identifiers
-var playerOne = "red";
-var playerTwo = "yellow";
+var playerOne = "Red";
+var playerTwo = "Yellow";
 var currentPlayer = playerOne;
 
 //game state variables
 var gameOver = false;
 var board;
+var currentColumns;
 
 //board dimensions
 var rows = 6;
 var columns = 7;
-
-
-socket.on("playerAssigned", (color) => {
-    playerColor = color;
-    console.log("You are: " + playerColor);
-    updateTurn();   //updates the UI based on the color assigned
-})
-
-socket.on("gameFull", () => {
-    alert("Game is full. Try again later.");
-    window.location.reload(); // Refresh the page if game is full
-});
-
-socket.on("updateGame", (gameData) => {
-    let {board: updatedBoard, row, col, currentPlayer: updatedPlayer} = gameData;
-    board = updatedBoard;
-
-    if (row >= 0 && col >= 0) {
-        let tile = document.getElementById(row.toString() + "-" + col.toString());
-        tile.classList.remove("hover-highlight-red", "hover-highlight-yellow");
-
-        if (updatedPlayer === "red") {
-            tile.classList.add("red-piece");
-        } else {
-            tile.classList.add("yellow-piece");
-        }
-        tile.classList.add("falling-piece");    //drop animation
-        
-        setTimeout(() => {
-            tile.classList.remove("falling-piece");
-            updateTurn();   //updates the turn display
-        }, 1000);
-    }
-
-    currentPlayer = updatedPlayer === "red" ? "yellow" : "red";
-    updateTurn();
-});
-
-socket.on("gameOver", (winnerData) => {
-    let winnerElement = document.getElementById("winner");
-    winnerElement.innerHTML = `<span class="${winnerData.winner === "red" ? "red-text" : "yellow-text"}">${winnerData.winner}</span> Wins!`;
-    gameOver = true;
-   // resetGame();
-});
-
-socket.on("resetGame", () => {
-    gameOver = false;
-    currentPlayer = "red";
-    document.getElementById("winner").innerText = "";
-    setGame();
-});
 
 //game initializers when the page loads
 window.onload = function() {
@@ -77,6 +21,7 @@ window.onload = function() {
 //functino to start/reset the game
 function setGame() {
     board = [];
+    currentColumns = [5, 5, 5, 5, 5, 5, 5]; //tracker for current available row for each column
 
     //clears board on reset
     document.getElementById("board").innerHTML = "";
@@ -105,14 +50,94 @@ function setGame() {
 
 //function to set a piece on the board when clicked
 function setPiece() {
-    if (gameOver || playerColor !== currentPlayer) { //prevents any actions to be done if the game is over
+    if (gameOver) { //prevents any actinos to be done if the game is over
         return;
     }
 
     let coordinates = this.id.split("-"); //gets row and column from tile ID
     let r = parseInt(coordinates[0]);
     let c = parseInt(coordinates[1]);
-    socket.emit("dropPiece", { row: r, col: c });    //emits the move to the server
+
+    r = currentColumns[c];  //gets the current available row for the clicked column
+    if (r < 0) {    //if the column is full, do nothing
+        return;
+    }
+
+    //places the piece then updates the board
+    board[r][c] = currentPlayer;
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+
+    tile.classList.remove("hover-highlight-red", "hover-highlight-yellow"); //removes hover highlight
+
+    //proper colored pieces with drop animation
+    if (currentPlayer == playerOne) {
+        tile.classList.add("red-piece");
+        currentPlayer = playerTwo;  //switches turn to player two
+    } else {
+        tile.classList.add("yellow-piece");
+        currentPlayer = playerOne;  //switches turn to player one
+    }
+    tile.classList.add("falling-piece");    //drop animation
+
+    setTimeout(() => {
+        tile.classList.remove("falling-piece");
+        updateTurn();   //updates the turn display
+        checkWinner();  //checks for winner
+    }, 1000);
+
+    r-=1;   //updates row height for column
+    currentColumns[c] = r;  //updates the current avaialble row for the column
+}
+
+//checks to see if a player has won
+function checkWinner() {
+    //horizontally
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                    setWinner(r,c); //if winner is found then set the winner
+                    return;
+                }
+            }
+        }
+    }
+
+    //vertically
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                    setWinner(r,c);
+                    return;
+                }
+            }
+        }
+    }
+
+    //diagonally (top left -> bottom right)
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns -3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                    setWinner(r,c);
+                    return;
+                }
+            }
+        }
+    }
+
+    //anti diagnonally (bottom left -> top right)
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                    setWinner(r,c);
+                    return;
+                }
+            }
+        }
+    }
 }
 
 //function to set/display the winner
@@ -121,7 +146,7 @@ function setWinner(r,c) {
 
     //if winner = playerOne -> red, else -> yellow
     let playerColor = board[r][c] === playerOne ? "red-text" : "yellow-text";
-    let playerName = board[r][c] === playerOne ? "red" : "yellow";
+    let playerName = board[r][c] === playerOne ? "Red" : "Yellow";
 
     //updates the display for the winner with the proper color
     winner.innerHTML = `<span class="${playerColor}">${playerName}</span> Wins!`;
@@ -131,23 +156,13 @@ function setWinner(r,c) {
 }
 
 
-//highlights only available index on board
-function getLowestEmptyRow(board, col) {
-    for (let row = board.length - 1; row >= 0; row--) {
-        if (board[row][col] === " ") {
-            return row;
-        }
-    }
-    return -1;
-}
-
 //function to highlight a tile whenever the mouse is hovered over it
 function highlightTile() {
     if (gameOver) {
         return;
     }
     let c = parseInt(this.id.split("-")[1]);  //gets column index
-    let r = getLowestEmptyRow(board, c);
+    let r = currentColumns[c];
 
     if (r >= 0) {
         let tile = document.getElementById(r.toString() + "-" + c.toString());
@@ -168,9 +183,9 @@ function highlightTile() {
 //function to remove the hover highlight whenever the mouse leaves the tile
 function removeHighlight() {
     let c = parseInt(this.id.split("-")[1]);
-    let r = getLowestEmptyRow(board, c);
+    let r = currentColumns[c];
 
-    if (r >= 0) {
+    if (r>= 0) {
         let tile = document.getElementById(r.toString() + "-" + c.toString());
         tile.classList.remove("hover-highlight-red", "hover-highlight-yellow");
     }
@@ -179,10 +194,10 @@ function removeHighlight() {
 //function to update player turn display and color
 function updateTurn() {
     let turnElement = document.getElementById("turn");
-    let playerColorStyle = `${currentPlayer}-text`;
-    let playerName = currentPlayer;
+    let playerColor = currentPlayer === playerOne ? "red-text" : "yellow-text";
+    let playerName = currentPlayer === playerOne ? "Red" : "Yellow";
 
-    turnElement.innerHTML = `Current Player Turn: <span class="${playerColorStyle}">${playerName}</span>`;
+    turnElement.innerHTML = `Current Player Turn: <span class="${playerColor}">${playerName}</span>`;
 }
 
 //function to create and display the reset button
@@ -203,6 +218,5 @@ function resetGame() {
     gameOver = false;
     currentPlayer = playerOne;
     document.getElementById("winner").innerText = "";   //clears the winner text
-    socket.emit("resetGame");
     setGame();  //re-starts the game
 }
